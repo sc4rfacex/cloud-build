93ad2603a15214954dbedf3057d9e8c59954443a
Anthony Lopez Arizmendi <antony.lopez@coppel.com>
2024-06-27 20:32:49 -0700

    add: py code

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end('<h1> Welcome to container world : Docker </h1>');
+}
+
+var www = http.createServer(handleRequest);
+www.listen(8080);
